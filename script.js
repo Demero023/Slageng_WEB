df3327b705ed3b9fc8e5bd20a457c75a98868a70
Demero023 <demero023@gmail.com>
2026-07-24 23:23:26 +0200

    Style css and start of Script

---
+++ script.js
@@ -0,0 +1,25 @@
+// Reference to the booking modal element (expected to have id="bookingModal")
+const modal = document.getElementById("bookingModal");
+
+// Button that opens the booking modal (expected to have id="bookNowBtn")
+const btn = document.getElementById("bookNowBtn");
+
+// Close button inside the modal (expected to have a class of "close-btn")
+const closeBtn = document.querySelector(".close-btn");
+
+// Open modal when clicked
+btn.onclick = function () {
+  modal.style.display = "block";
+};
+
+// Close the modal
+closeBtn.onclick = function () {
+  modal.style.display = "none";
+};
+
+// Close the modal when clicking out side
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
